7c5cb306d4bd53866086384e36898777d4e4d0b9
Maria <venema.jms@gmail.com>
2018-12-03 15:25:32 +0700

    Ограничение на перемещение окна (криво работает)

--- js/dialog.js
+++ js/dialog.js
@@ -3,9 +3,33 @@
 var setupDialogElement = document.querySelector('.setup');
 var dialogHandler = setupDialogElement.querySelector('.upload');
 
+var limits = {
+  top: document.documentElement.offsetTop,
+  right: document.documentElement.offsetWidth - setupDialogElement.offsetWidth,
+  bottom: document.documentElement.offsetHeight - setupDialogElement.offsetHeight,
+  left: document.documentElement.offsetLeft
+};
+
+var getValueInLimit = function (value, min, max) {
+  if (value < min) {
+    value = min;
+  } if (value > max) {
+    value = max;
+  }
+  return value;
+};
+
+var setupCoords = function (coords) {
+  coords.x = getValueInLimit(coords.x, limits.left, limits.right);
+  coords.y = getValueInLimit(coords.y, limits.bottom, limits.top);
+
+  return coords;
+};
+
 dialogHandler.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
 
+  // координаты курсора мыши на момент начала движения
   var startCoords = {
     x: evt.clientX,
     y: evt.clientY
@@ -17,18 +41,27 @@ dialogHandler.addEventListener('mousedown', function (evt) {
     moveEvt.preventDefault();
     dragged = true;
 
+    // смещение
     var shift = {
       x: startCoords.x - moveEvt.clientX,
       y: startCoords.y - moveEvt.clientY
     };
 
+    // текущие координаты курсора
     startCoords = {
       x: moveEvt.clientX,
       y: moveEvt.clientY
     };
 
-    setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
-    setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
+    var resultCoords = {
+      x: setupDialogElement.offsetLeft - shift.x,
+      y: setupDialogElement.offsetTop - shift.y
+    };
+
+    setupCoords(resultCoords);
+
+    setupDialogElement.style.top = resultCoords.y + 'px';
+    setupDialogElement.style.left = resultCoords.x + 'px';
 
   };
 
