1cc598a9880f40926205bc9ad7b125f860f6199f
Maria <venema.jms@gmail.com>
2018-12-17 13:04:47 +0700

    eslint

--- js/photo.js
+++ js/photo.js
@@ -10,7 +10,7 @@
     var fileName = file.name.toLowerCase();
 
     var matches = FILE_TYPES.some(function (it) {
-     return fileName.endsWith(it);
+      return fileName.endsWith(it);
     });
 
     if (matches) {
